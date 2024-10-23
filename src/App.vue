b2539790aa2abafddf7a3a5480c43341b5567232
Quoc Le <quocle152@gmail.com>
2024-10-23 17:59:13 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container flex flex-col gap-3 justify-center items-center py-8">
+    <div class="flex flex-col gap-2">
+      <label class="text-2xl">{{ text }}</label>
+      <InputChild v-model="text"></InputChild> 
+    </div>
+    <div class="flex flex-col gap-2">
+      <p>shadcn</p>
+      <Input v-model="text"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { Input } from '@/components/ui/input'
+import InputChild from '@/components/input-child/Input-child.vue'; 
+
+let text = ref('Hello World!'); 
+</script>
---
+++ src/components/input-child/Input-child.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <input 
+            class="border border-black rounded-sm" 
+            type="text" 
+            :value="modelValue" 
+            @input="onInput" 
+        />
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    modelValue: {
+        type: String,
+        required: true,
+    },
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+// Hàm xử lý sự kiện input
+const onInput = (event) => {
+    emit('update:modelValue', event.target.value); 
+};
+</script>
